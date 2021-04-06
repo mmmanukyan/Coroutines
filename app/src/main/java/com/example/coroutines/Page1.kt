4efd51627a3f64640b65d72e1050edcd9dc3f2dd
mmmanukyan <mmmanukyan@live.com>
2021-04-07 00:22:59 +0400

    code improvement 2

--- app/src/main/java/com/example/coroutines/Page1.kt
+++ app/src/main/java/com/example/coroutines/Page1.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.coroutines.databinding.FragmentPage1Binding
 
@@ -13,14 +14,13 @@ class Page1 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragment = this
         return FragmentPage1Binding.inflate(inflater, container, false).apply {
             btn.setOnClickListener {
                 if(!editText.text.isNullOrEmpty() && !editText2.text.isNullOrEmpty()) {
                     val count = editText.text.toString().toInt()
                     val max = editText2.text.toString().toInt()
                     if (max > 0 && count in 0..max) {
-                        NavHostFragment.findNavController(fragment).navigate(Page1Directions.actionPage1ToPage2(count,max))
+                        it.findNavController().navigate(Page1Directions.actionPage1ToPage2(count,max))
                     }
                 }
             }
